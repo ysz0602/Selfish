13dad93e0ad520613974419255f740982ac25fee
ysz <1582945670@qq.com>
2019-08-06 00:24:07 +0800

    新增接口

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
+
+// 引入users.js
+const users = require('./routes/api/user')
+
+// DB config
+const db = require('./config/keys').mongoURI
+
+// 使用body-parser中间件
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Connect to mongodb
+mongoose.connect(db)
+    .then(() => console.log("MongoDB Connected."))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send("hello world")
 })
+
+// 使用routes
+app.use('/api/users', users)
 
 const port = process.env.PORT || 5000
 
